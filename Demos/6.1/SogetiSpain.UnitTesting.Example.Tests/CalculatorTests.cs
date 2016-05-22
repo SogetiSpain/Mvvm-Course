ac902739dd781314736df1abefcefa5734c632f4
Óscar Fernández González <ofgnet@msn.com>
2016-05-22 13:18:31 +0200

    Add Demo: Simple Example of Unit Testing

---
+++ Demos/6.1/SogetiSpain.UnitTesting.Example.Tests/CalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿namespace SogetiSpain.UnitTesting.Example.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddShouldReturnSumOfValues()
+        {
+            // Arrange -> "Disponer"
+            var calculator = new Calculator();
+
+            // Act -> "Actuar"
+            int result = calculator.Add(6, 2);
+
+            // Assert -> "Afirmar"
+            Assert.AreEqual(8, result);
+        }
+    }
+}
---
+++ Demos/6.1/SogetiSpain.UnitTesting.Example/Calculator.cs
@@ -0,0 +1,12 @@
+﻿namespace SogetiSpain.UnitTesting.Example
+{
+    using System;
+
+    public class Calculator
+    {
+        public int Add(int firstValue, int secondValue)
+        {
+            return firstValue + secondValue;
+        }
+    }
+}
